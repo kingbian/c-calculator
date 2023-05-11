38ffe01f02088dc444d0795bec0a5c556be7c7af
king <kingbian72@gmail.com>
2023-05-10 23:05:12 -0400

    intial commit

---
+++ compute.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int compute(char input[]) {
+
+    // convert the asci values into an int
+    int a = input[0] - '0'; 
+    int b = input[1] - '0';
+
+    return  a+b;
+
+}
+
+    void main() {
+        char input[10];
+        printf("Enter a equation: ");
+        scanf("%s", input);
+        
+        int result = compute(input);
+
+        printf("your answer is: %d\n", result);
+    }
